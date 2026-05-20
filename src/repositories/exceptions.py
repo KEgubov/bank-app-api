0a9eb2336466591c3b12f63f5e7c80238c42af5a
KEgubov <kirilegubov@gmail.com>
2026-05-20 22:03:42 +0200

    feat: added a custom error to handle duplicate errors

--- src/repositories/exceptions.py
+++ src/repositories/exceptions.py
@@ -1,5 +1,25 @@
+from sqlalchemy.exc import IntegrityError
+
+
 class RepositoryError(Exception):
     def __init__(self, message, error_code: str = "VALIDATION_ERROR"):
         self.message = message
         self.error_code = error_code
         super().__init__(message)
+
+
+class DuplicateError(IntegrityError):
+    def __init__(
+        self,
+        message,
+        error_code: str = "DUPLICATE_ERROR",
+        orig: Exception | None = None,
+        params: tuple | None = None,
+        statement: str | None = None,
+    ):
+        self.message = message
+        self.error_code = error_code
+        super().__init__(statement, params, orig)
+
+    def __str__(self) -> str:
+        return f"{self.error_code}: {self.message}"
